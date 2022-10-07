0f3ee39ed99924203bf3a80dc7acb625902a0d6d
Paola Ruíz <pruiz@lambda003.uniandes.edu.co>
2022-10-07 12:45:32 -0500

    AMPs-Net

--- run_multilabel.sh
+++ run_multilabel.sh
@@ -1,10 +1,11 @@
 EXPERIMENT='TEST_MULTILABEL_AMP'
 BS=64
 DEVICE=4
-python main.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --cross_val 1 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4 --lr 5e-5 --balanced_loader --metadata
-python main.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --cross_val 2 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4 --lr 5e-5 --balanced_loader --metadata
-python main.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --cross_val 3 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4 --lr 5e-5 --balanced_loader --metadata
-python main.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --cross_val 4 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4 --lr 5e-5 --balanced_loader --metadata
+EPOCHS=300
+python main2.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --cross_val 1 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4 --lr 5e-5 --balanced_loader --metadata --epochs $EPOCHS
+python main.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --cross_val 2 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4 --lr 5e-5 --balanced_loader --metadata --epochs $EPOCHS
+python main.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --cross_val 3 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4 --lr 5e-5 --balanced_loader --metadata --epochs $EPOCHS
+python main.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --cross_val 4 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4 --lr 5e-5 --balanced_loader --metadata --epochs $EPOCHS
 python validation.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4
 python train.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4
 python test.py --device $DEVICE --use_gpu --conv_encode_edge --block res+ --gcn_aggr softmax --learn_t --t 1.0 --dropout 0.2 --batch_size $BS --save $EXPERIMENT --multilabel --nclasses 4
